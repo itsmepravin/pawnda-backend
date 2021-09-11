3b168afec069b96e33bee347b15be02a1dbf56c5
Pravin Singh <singh.pravin19@yahoo.com>
2021-09-11 11:29:01 +0545

    First Commit

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ import express from'express'
 import mongoose from 'mongoose'
 import cors from 'cors'
 import asyncErrors from 'express-async-errors'
+import path from 'path'
+import { dirname } from 'path'
+import { fileURLToPath } from 'url'
 
 import dogsRouter from './routes/dogsRouter.js'
 import userRouter from './routes/userRouter.js'
@@ -13,6 +16,8 @@ asyncErrors
 
 const app = express()
 
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
 morgan.token('body', (req, res) => JSON.stringify(req.body))
 
 mongoose.connect(MONGODB_URI, {useNewUrlParser:true, useUnifiedTopology:true, useFindAndModify:false, useCreateIndex:true})
@@ -20,15 +25,18 @@ mongoose.connect(MONGODB_URI, {useNewUrlParser:true, useUnifiedTopology:true, us
 .catch(error => console.log(`Failed to connect to the database!`, error.message))
 
 app.use(cors())
-app.use(express.static('build'))
 
 app.use(express.json({ limit: "30mb", extended: true}))
 app.use(express.urlencoded({ limit: "30mb", extended: true}))
 
 app.use(morgan(':method :url :status :res[content-length] :response-time ms :body'))
 
-app.use('/api/dogs', dogsRouter)
-app.use('/api/user', userRouter)
+app.use('/dogs', dogsRouter)
+app.use('/user', userRouter)
+
+app.get('/', (req, res) => {
+	res.send('<h1>Welcome to the PAWnda website</h1>')
+})
 
 app.use(unknownEndpoint)
 app.use(errorHandler)
